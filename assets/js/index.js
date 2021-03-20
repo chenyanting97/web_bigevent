a5ef01cd958248cb236cbb321517db2c98d864ef
chenyanting <1353861493@qq.com>
2021-03-20 15:02:05 +0800

    后台主页

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,6 +9,13 @@ function getUserInfo() {
     // 获取头部配置对象
     headers: {
       Authorization: localStorage.getItem('token')
+    },
+    success: function (res) {
+      if (res.status !== 0) {
+        return console.log('获取用户数据失败');
+      }
+      console.log('获取用户数据成功' + res);
+
     }
   })
 }
